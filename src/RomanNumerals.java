cf1362e00a1c3a288948e2896969881fb3b510ff
somename <someemail>
2016-09-30 16:26:14 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -2,11 +2,13 @@
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 		if (romanNum.length() == 1) {
-			return getArabNumForBasicRN(romanNum);
+			return getArabNumForBasicRN(romanNum.charAt(0));
 		} else if (romanNum.length() == 2) {
 			char[] digits = splitRN(romanNum);
 			if (getArabNumForBasicRN(digits[0]) < getArabNumForBasicRN(digits[1])) {
-				return getArabNumForBasicRN(digits[0] + getArabNumForBasicRN(digits[1]));
+				return getArabNumForBasicRN(digits[0]) + getArabNumForBasicRN(digits[1]);
+			} else {
+				return getArabNumForBasicRN(digits[1]) - getArabNumForBasicRN(digits[0]);
 			}
 			if (romanNum.equals("II"))
 				return 2;
@@ -21,19 +23,19 @@ public class RomanNumerals {
 	}
 	
 	public int getArabNumForBasicRN(char rnDigit) {
-		if (rnDigit.equals('I'))
+		if (rnDigit == 'I')
 			return 1;
-		else if (rnDigit.equals('V'))
+		else if (rnDigit == 'V')
 			return 5;
-		else if (rnDigit.equals('X'))
+		else if (rnDigit == 'X')
 			return 10;
-		else if (rnDigit.equals('L'))
+		else if (rnDigit == 'L')
 			return 50;
-		else if (rnDigit.equals('C'))
+		else if (rnDigit == 'C')
 			return 100;
-		else if (rnDigit.equals('D'))
+		else if (rnDigit == 'D')
 			return 500;
-		else if (rnDigit.equals('M'))
+		else if (rnDigit == 'M')
 			return 1000;
 		
 		return 0;
